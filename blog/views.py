42820eea8f2be1cbcb9c1cf799891692aa150db3
Akash <adraut93@gmail.com>
2020-08-04 10:24:06 +0530

    Super user created, Post models created, views.py file updated for dynamic data

--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,10 @@
 from django.shortcuts import render
-
-
-posts = [
-    {
-        'author': 'Akash Raut',
-        'title': 'Blog Post1',
-        'content': 'First Post Content',
-        'date_posted': 'August 02, 2020'
-    },
-    {
-        'author': 'Plash Raut',
-        'title': 'Blog Post2',
-        'content': 'Second Post Content',
-        'date_posted': 'August 02, 2020'
-    }
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
