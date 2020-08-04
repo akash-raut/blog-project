efbfa761a630581a0d86f1354b8505001496ea99
Akash <adraut93@gmail.com>
2020-08-04 14:48:15 +0530

    User Registration form created, crispy form added, message alerts added

--- blog_project/urls.py
+++ blog_project/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from users import views as user_views  # Can also route through by adding urls.py file to users app
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('register/', user_views.register, name='register'),
     path('', include('blog.urls')),
 ]
